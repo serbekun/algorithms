b635de282fda026e851c82d9e21ac082f9eee93a
serbekun <sergey994049@gmail.com>
2025-06-11 21:18:55 +0900

    update

--- sekishow.c
+++ sekishow.c
@@ -2,139 +2,142 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define COLS 6
+#define COLS        6
 #define PLACES_SIZE 25
-#define FILENAME "new_sit_place.txt"
+#define FILENAME    "new_sit_place.txt"
 
-/* funk for show result */
+typedef struct {
+    int id;
+    int sex;  // 0=boy, 1=girl
+    int eyes; // 0=good, 1=bad
+} student;
+
 int ShowResult() {
+    FILE *f = fopen(FILENAME, "r");
+    if (!f) { perror("Error opening file"); return 1; }
 
-    // open file
-    FILE *file = fopen(FILENAME, "r");
-    if (!file) { // if error open file return
-        perror("error open file from argv[1]");
-        return 1;
-    }
-
-    // init value
     int row, col, student;
     char gender;
     int count = 0;
-
-    // read file and print
-    while (fscanf(file, "Row %d, Col %d: Student %d (%c)\n", &row, &col, &student, &gender) == 4) {
+    while (fscanf(f, "Row %d, Col %d: Student %d (%c)\n",
+                  &row, &col, &student, &gender) == 4) {
         printf("[%d (%c)] ", student, gender);
-        count++;
-
-        if (count % COLS == 0) {
-            printf("\n");
-        }
+        if (++count % COLS == 0) printf("\n");
     }
-
-    if (count % COLS != 0) {
-        printf("\n");
-    }
-
-    // clease file return 0
-    fclose(file);
+    if (count % COLS) printf("\n");
+    fclose(f);
     return 0;
 }
 
-struct student {
-    int id;
-    int sex; // 0 - boy, 1 - girl
-};
-
-void shuffle(int *array, int size) {
-    for (int i = size - 1; i > 0; i--) {
+void shuffle(int *a, int n) {
+    for (int i = n - 1; i > 0; i--) {
         int j = rand() % (i + 1);
-        int tmp = array[i];
-        array[i] = array[j];
-        array[j] = tmp;
+        int t = a[i]; a[i] = a[j]; a[j] = t;
     }
 }
 
 int get_required_sex(int row, int col) {
-    if (row % 2 == 0)
-        return (col % 2 == 0) ? 0 : 1;
-    else
-        return (col % 2 == 0) ? 1 : 0;
+    return (row % 2 == 0) ? (col % 2 == 0 ? 0 : 1) : (col % 2 == 0 ? 1 : 0);
 }
 
-void SetPlaces(struct student **students, int *places, int size, int cols) {
+void SetPlaces(student **students, int *places, int size, int cols) {
     int rows = (size + cols - 1) / cols;
-    int *boys = malloc(size * sizeof(int));
-    int *girls = malloc(size * sizeof(int));
-    int boy_count = 0, girl_count = 0;
+
+    int bad_boys[size], bad_girls[size], good_boys[size], good_girls[size];
+    int bb=0, bg=0, gb=0, gg=0;
 
     for (int i = 0; i < size; i++) {
-        if (students[i]->sex == 0)
-            boys[boy_count++] = students[i]->id;
-        else
-            girls[girl_count++] = students[i]->id;
-    }
-
-    shuffle(boys, boy_count);
-    shuffle(girls, girl_count);
-
-    int boy_idx = 0, girl_idx = 0;
-    for (int i = 0; i < size; i++) places[i] = 0;
-
-    for (int row = 0; row < rows; row++) {
-        int row_cols = (row < rows - 1) ? cols : size - (rows - 1) * cols;
-        for (int col = 0; col < row_cols; col++) {
-            int idx = row * cols + col;
-            int sex = get_required_sex(row, col);
-            if (sex == 0 && boy_idx < boy_count) {
-                places[idx] = boys[boy_idx++];
-            } else if (sex == 1 && girl_idx < girl_count) {
-                places[idx] = girls[girl_idx++];
-            } else if (boy_idx < boy_count) {
-                places[idx] = boys[boy_idx++];
-            } else if (girl_idx < girl_count) {
-                places[idx] = girls[girl_idx++];
-            }
+        student *s = students[i];
+        if (s->eyes == 1) {
+            (s->sex == 0) ? (bad_boys[bb++] = s->id) : (bad_girls[bg++] = s->id);
+        } else {
+            (s->sex == 0) ? (good_boys[gb++] = s->id) : (good_girls[gg++] = s->id);
         }
     }
 
-    free(boys);
-    free(girls);
+    shuffle(bad_boys, bb);
+    shuffle(bad_girls, bg);
+    shuffle(good_boys, gb);
+    shuffle(good_girls, gg);
+
+    for (int i = 0; i < size; i++) places[i] = 0;
+
+    int ibb=0, ibg=0, igb=0, igg=0;
+
+    for (int r = 0; r < rows; r++) {
+        int row_cols = (r < rows - 1) ? cols : size - (rows - 1) * cols;
+        for (int c = 0; c < row_cols; c++) {
+            int idx = r * cols + c;
+            if (places[idx] != 0) continue;
+
+            int need = get_required_sex(r, c);
+            if (need == 0) {
+                places[idx] = (ibb < bb) ? bad_boys[ibb++] :
+                              (igb < gb) ? good_boys[igb++] :
+                              (ibg < bg) ? bad_girls[ibg++] :
+                                           good_girls[igg++];
+            } else {
+                places[idx] = (ibg < bg) ? bad_girls[ibg++] :
+                              (igg < gg) ? good_girls[igg++] :
+                              (ibb < bb) ? bad_boys[ibb++] :
+                                           good_boys[igb++];
+            }
+        }
+    }
 }
 
 int main() {
-    srand(time(NULL));
-    int *places = malloc(PLACES_SIZE * sizeof(int));
+    srand((unsigned)time(NULL));
 
-    struct student s[PLACES_SIZE] = {
-        {1,1}, {2,1}, {3,1}, {4,1}, {5,0}, {6,0}, {7,1}, {8,0}, {9,0}, {10,0},
-        {11,0}, {12,0}, {13,1}, {14,1}, {15,1}, {16,0}, {17,0}, {18,0}, {19,0}, {20,0},
-        {21,1}, {22,1}, {23,0}, {24,1}, {25,1}
+    student s1 = {1, 1, 1};
+    student s2 = {2, 1, 1};
+    student s3 = {3, 1, 1};
+    student s4 = {4, 0, 1};
+    student s5 = {5, 0, 1};
+    student s6 = {6, 0, 0};
+    student s7 = {7, 1, 0};
+    student s8 = {8, 0, 0};
+    student s9 = {9, 0, 0};
+    student s10 = {10, 0, 0};
+    student s11 = {11, 0, 0};
+    student s12 = {12, 0, 0};
+    student s13 = {13, 1, 0};
+    student s14 = {14, 1, 0};
+    student s15 = {15, 1, 0};
+    student s16 = {16, 0, 0};
+    student s17 = {17, 0, 0};
+    student s18 = {18, 0, 0};
+    student s19 = {19, 0, 0};
+    student s20 = {20, 0, 0};
+    student s21 = {21, 1, 0};
+    student s22 = {22, 1, 0};
+    student s23 = {23, 0, 0};
+    student s24 = {24, 1, 0};
+    student s25 = {25, 1, 0};
+
+    student *persons[PLACES_SIZE] = {
+        &s1, &s2, &s3, &s4, &s5, &s6, &s7, &s8, &s9, &s10,
+        &s11, &s12, &s13, &s14, &s15, &s16, &s17, &s18, &s19, &s20,
+        &s21, &s22, &s23, &s24, &s25
     };
 
-    struct student *ptrs[PLACES_SIZE];
-    for (int i = 0; i < PLACES_SIZE; i++) {
-        ptrs[i] = &s[i];
-    }
+    int *places = malloc(PLACES_SIZE * sizeof(int));
+    if (!places) { perror("malloc"); return 1; }
 
-    SetPlaces(ptrs, places, PLACES_SIZE, COLS);
+    SetPlaces(persons, places, PLACES_SIZE, COLS);
 
-    FILE *file = fopen(FILENAME, "w");
-    if (!file) {
-        perror("Can't open file");
-        return 1;
-    }
+    FILE *f = fopen(FILENAME, "w");
+    if (!f) { perror("fopen"); free(places); return 1; }
 
     for (int i = 0; i < PLACES_SIZE; i++) {
-        int row = i / COLS;
-        int col = i % COLS;
-        fprintf(file, "Row %d, Col %d: Student %d (%s)\n",
-            row + 1, col + 1, places[i],
-            (ptrs[places[i] - 1]->sex == 0) ? "M" : "F");
+        int r = i / COLS, c = i % COLS;
+        int id = places[i];
+        char *g = (persons[id-1]->sex == 0) ? "M" : "F";
+        fprintf(f, "Row %d, Col %d: Student %d (%s)\n", r+1, c+1, id, g);
     }
-    fclose(file);
-    
-    printf("Done. See '%s'\n", FILENAME);
-    free(places);
+    fclose(f);
+
     ShowResult();
+    free(places);
     return 0;
 }
